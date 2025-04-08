d4d3f4bbe96869890a0c70e63e4a05e5af105176
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-04-08 06:05:06 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,60 @@
+import java.io.*;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String readFile(String nameOfFile){
+		FileReader inputStream = null;
+			String result = "";
+				try{
+			inputStream = new FileReader(nameOfFile);
+			int x;
+			while((x = inputStream.read()) != -1){
+			results += (char)(x);
+			}
+		}
+			catch(IOException e){
+			System.out.println(e);
+		}
+			finally{
+			try{
+			if(inputStream != null){
+			inputStream.close();
+			}
+		}
+			catch(IOException e){
+			System.out.println(e);
+		}
+	}
+		return results;
+	}
 
+	public static String backwardsReadFile(String nameOfFile){
+		FileReader inputStream = null;
+		String result = "";
+		try{
+		inputStream = new FileReader(nameOfFile);
+		int x;
+		while((x = inputStream.read()) != -1){
+	results = (char)(x) + result;
+			}
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally{
+		try{
+		if(inputStream != null){
+		inputStream.close();
+				}
+			}
+		catch(IOException e){
+		System.out.println(e);
+			}
+		}
+	return results;
+	}
 }
